03880ca57accff65d98cd505ee2789e06abf6a56
thekaroe-thailand <thekaroe@gmail.com>
2024-09-05 21:27:55 +0700

    day3

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
+const userController = require("./controllers/UserController");
 
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/api/user/signIn", (req, res) => userController.signin(req, res));
 
 app.listen(3000, () => {
   console.log("API Server Running...");
